706fe54cada17404d7ed43e19809628c305b5eaa
worscik <58079969+worscik@users.noreply.github.com>
2022-11-19 21:17:07 +0100

    First commit
    Adding a mechanism for adding words. Test endpoint for the answer

---
+++ src/main/java/words/app/WebController.java
@@ -0,0 +1,42 @@
+package words.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+
+
+@RestController
+public class WebController {
+
+    @GetMapping("/")
+    public ModelAndView mainTheme(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index.html");
+        return modelAndView;
+    }
+
+
+    @GetMapping("/user1")
+    public HashMap<String,String> startGame() throws FileNotFoundException {
+        HashMap<String,String> map = new HashMap<>();
+        CreateSetForUser createSetForUser = new CreateSetForUser(map);
+        createSetForUser.addToMap();
+        Answer answer = new Answer();
+        answer.checkAnswer(createSetForUser);
+        return map;
+    }
+
+    @GetMapping("/user2")
+    public HashMap<String,String> startGame1() throws FileNotFoundException {
+        HashMap<String,String> map = new HashMap<>();
+        CreateSetForUser createSetForUser = new CreateSetForUser(map);
+        createSetForUser.addToMap();
+        Answer answer = new Answer();
+        answer.checkAnswer(createSetForUser);
+        return map;
+    }
+
+}
